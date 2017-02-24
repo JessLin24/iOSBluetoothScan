3b45248ae864930228829656de3dec71c8a8a615
Guest User <Guest@ryans-mbp.lan>
2017-02-23 23:07:01 -0600

    Initial Commit

---
+++ iOSBluetoothScan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSBluetoothScan
+//
+//  Created by Guest User on 2/23/17.
+//  Copyright © 2017 walkingbus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
