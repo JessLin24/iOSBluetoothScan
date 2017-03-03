0aedea39f7fe3d44e3c1a90237a16e807e822c66
Ryan Ding <rding@ryans-macbook-pro.local>
2017-03-02 23:09:59 -0600

    Updated for ManufacturerSpecificData protocol data extraction

--- iOSBluetoothScan/ViewController.swift
+++ iOSBluetoothScan/ViewController.swift
@@ -19,11 +19,28 @@ class ViewController: UIViewController {
 
         //Initialise CoreBluetooth Central Manager
         centralManager = CBCentralManager(delegate: self, queue: DispatchQueue.main)
-        expectedTags.append("CB4C2E61-FEF3-47FF-8AEC-67A9B883016C")
+        //expectedTags.append("CB4C2E61-FEF3-47FF-8AEC-67A9B883016C")
+        expectedTags.append("WalkingBus")
     }
 }
 
 extension ViewController: CBCentralManagerDelegate {
+    func startScanning(){
+        let scanPeriod = 5
+        print("Scan Started. Scan period of \(scanPeriod)")
+        self.centralManager?.scanForPeripherals(withServices : nil, options: [CBCentralManagerScanOptionAllowDuplicatesKey: true])
+        self.perform(#selector(stopScanning), with: self, afterDelay: Double(scanPeriod))
+        //stopScanning()
+    }
+    
+    func stopScanning(){
+        print("\nScan Stopped")
+        print("There are \(peripherals.count) of expected tags found")
+        print("Expected tags that were found \(peripherals)\n")
+        peripherals.removeAll()
+        startScanning()
+    }
+
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         if (central.state == .poweredOn){
             startScanning()
@@ -34,24 +51,31 @@ extension ViewController: CBCentralManagerDelegate {
     }
     
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        if(expectedTags.contains(peripheral.identifier.uuidString)
-            && !peripherals.contains(peripheral)){
-            print("New expected tag found! \(peripheral)")
-            peripherals.append(peripheral)
+        if let pName = peripheral.name{
+            if(expectedTags.contains(pName) && !peripherals.contains(peripheral)){
+                print("\nNew expected tag found! \(peripheral)")
+                peripherals.append(peripheral)
+                if let manufacturerData = advertisementData["kCBAdvDataManufacturerData"] as? Data{
+                    assert(manufacturerData.count>=7)
+                    
+                    //0d00 - TI manufacturer ID - 2 byte
+                    var manufacturerID = String(format: "%02X", (manufacturerData[0]))
+                    manufacturerID += String(format: "%02X", (manufacturerData[1]))
+                    print("Manufacturer ID: \(manufacturerID)")
+                    
+                    //6 byte MAC address
+                    var MACAddress = String(format: "%02X", manufacturerData[2])
+                    for i in 3...7  {
+                        MACAddress += ":"
+                        MACAddress += String(format: "%02X", manufacturerData[i])
+                    }
+                    print("MAC Address: \(MACAddress)")
+                    
+                    //1 byte battery level
+                    let batteryLevel = Int(manufacturerData[8])
+                    print("Battery Level: \(batteryLevel)%")
+                }
+            }
         }
-    }
-    func startScanning(){
-        let scanPeriod = 5
-        print("Scan Started. Scan period of \(scanPeriod)")
-        self.centralManager?.scanForPeripherals(withServices : nil, options: [CBCentralManagerScanOptionAllowDuplicatesKey: true])
-        self.perform(#selector(stopScanning), with: self, afterDelay: Double(scanPeriod))
-    }
-    
-    func stopScanning(){
-        print("Scan Stopped")
-        print("There are \(peripherals.count) of expected tags found")
-        print("Expected tags that were found \(peripherals)")
-        peripherals.removeAll()
-        startScanning()
     }
 }
